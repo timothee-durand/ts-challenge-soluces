777521b7b05a07f0df807fa271bab25414935844
timothee-durand <timothee.durand@digitevent.com>
2023-05-12 15:25:55 +0200

    correct 11

--- soluces/11.ts
+++ soluces/11.ts
@@ -20,30 +20,29 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToObject<T extends readonly string[]> = {
-    [Properties in keyof T as T[Properties]]: T[Properties]
-  }
-  
-  /* _____________ Test Cases _____________ */
-  import type { Equal, Expect } from '@type-challenges/utils'
-  
-  const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
-  const tupleNumber = [1, 2, 3, 4] as const
-  const tupleMix = [1, '2', 3, '4'] as const
-  
-  type cases = [
-    Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
-    Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
-    Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
-  ]
-  
-  // @ts-expect-error
-  type error = TupleToObject<[[1, 2], {}]>
-  
-  /* _____________ Further Steps _____________ */
-  /*
-    > Share your solutions: https://tsch.js.org/11/answer
-    > View solutions: https://tsch.js.org/11/solutions
-    > More Challenges: https://tsch.js.org
-  */
-  
+type TupleToObject<T extends readonly (string | number)[]> = {
+  [key in T[number]]: key
+}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const tupleNumber = [1, 2, 3, 4] as const
+const tupleMix = [1, '2', 3, '4'] as const
+
+type cases = [
+  Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
+]
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/11/answer
+  > View solutions: https://tsch.js.org/11/solutions
+  > More Challenges: https://tsch.js.org
+*/
